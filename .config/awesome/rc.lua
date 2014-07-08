d0e926fcfc46ebdc165e6e7857865a89375b022a
Quentin Pradet <quentin.pradet@gmail.com>
2014-07-08 14:31:45 +0200

    Use xscreensaver instead of xlock

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -244,7 +244,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
-    awful.key({                   }, "F12", function () awful.util.spawn("xlock -mode voters") end),
+    awful.key({                   }, "F12", function () awful.util.spawn("xscreensaver-command -lock") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
