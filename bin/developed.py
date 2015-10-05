a6f7092006b8a839bf95972b1f298c3ac7d2f840
Quentin Pradet <quentin@clustree.com>
2015-10-05 15:15:38 +0400

    Show 'developed' for all Python versions
    Not only 3.4.

--- bin/developed.py
+++ bin/developed.py
@@ -7,6 +7,6 @@ from glob import glob
 project = os.path.basename(sys.argv[1])
 glob_path = os.path.join(
     os.environ['HOME'], '.virtualenvs', project,
-    'lib/python3.4/site-packages/*.egg-link')
+    'lib/python*.*/site-packages/*.egg-link')
 
 exit(not any([link for link in glob(glob_path)]))
